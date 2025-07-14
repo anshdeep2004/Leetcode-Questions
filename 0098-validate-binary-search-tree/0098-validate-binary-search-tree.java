474e00f6d8f141853d2fb255c16fdcfc0f5bdf13
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-07-14 18:22:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,37 +13,50 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         List<Integer> arr = new ArrayList<>();
+//         TreeNode curr = root;
+//         while(curr != null) {
+//             if(curr.left == null) {
+//                 arr.add(curr.val);
+//                 curr = curr.right;
+//             }
+//             else {
+//                 TreeNode temp = curr.left;
+//                 while(temp.right != null && temp.right != curr) {
+//                     temp = temp.right;
+//                 }
+//                 if(temp.right == null) {
+//                     temp.right = curr;
+//                     curr = curr.left;
+//                 }
+//                 else {
+//                     temp.right = null;
+//                     arr.add(curr.val);
+//                     curr = curr.right;
+//                 }
+//             }
+//         }
+//         int n = arr.size();
+//         for(int i=0; i<n-1; i++) {
+//             if(arr.get(i) >= arr.get(i+1)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+// The above code is good to go as we are using an inorder traversal to go through the tree to and then check if the inorder traversal is in acsending order. But its takes O(N) time complexity everytime. The below code is not taking time O(N) everytime as it is traversing and as soon as a unwanted node is found then it returns true. The idea is to carry the range for every Node and check if the node liye in that range.
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        TreeNode curr = root;
-        while(curr != null) {
-            if(curr.left == null) {
-                arr.add(curr.val);
-                curr = curr.right;
-            }
-            else {
-                TreeNode temp = curr.left;
-                while(temp.right != null && temp.right != curr) {
-                    temp = temp.right;
-                }
-                if(temp.right == null) {
-                    temp.right = curr;
-                    curr = curr.left;
-                }
-                else {
-                    temp.right = null;
-                    arr.add(curr.val);
-                    curr = curr.right;
-                }
-            }
-        }
-        int n = arr.size();
-        for(int i=0; i<n-1; i++) {
-            if(arr.get(i) >= arr.get(i+1)) {
-                return false;
-            }
-        }
-        return true;
+        return checkRangeInBST(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+    public boolean checkRangeInBST(TreeNode root, int maxVal, int minVal) {
+        if(root == null) return true;
+        if(root.val >= maxVal || root.val <= minVal) return false;
+        return checkRangeInBST(root.left, root.val, minVal) && checkRangeInBST(root.right, maxVal, root.val);
     }
 }
