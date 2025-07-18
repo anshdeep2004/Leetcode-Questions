fd973c7cbad045a09236f300550aea304d5ec2be
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-07-18 18:07:31 +0530

    Time: 5 ms (77.31%), Space: 46.1 MB (54.35%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,46 +1,36 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // The approach to this question is that we know that this is making a directed graph type of a thing. So the simple solution would be that it is making a cycle if the tasks are impossible to perform and if there is no cycle then thats possible to do these tasks and return the order.
-        // Step 1: Build the graph and in-degree array
-        List<List<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         int[] inDegree = new int[numCourses];
+        Queue<Integer> que = new LinkedList<>();
+        int[] result = new int[numCourses];
 
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
+        for(int i=0; i<numCourses; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prereq = pair[1];
-            graph.get(prereq).add(course);
-            inDegree[course]++;
+        for(int i=0; i<prerequisites.length; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
         }
 
-        // Step 2: Initialize the queue with all courses having in-degree 0
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                queue.offer(i);
+        for(int i=0; i<numCourses; i++) {
+            if(inDegree[i] == 0) {
+                que.offer(i);
             }
         }
 
-        // Step 3: Topological Sort using BFS
-        int[] order = new int[numCourses];
-        int idx = 0;
-
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            order[idx++] = course;
-
-            for (int neighbor : graph.get(course)) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    queue.offer(neighbor);
+        int j = 0;
+        while(!que.isEmpty()) {
+            int node = que.poll();
+            result[j++] = node;
+            for(int i : adj.get(node)) {
+                inDegree[i]--;
+                if(inDegree[i] == 0) {
+                    que.offer(i);
                 }
             }
         }
-
-        // Step 4: Check if we could schedule all courses
-        return idx == numCourses ? order : new int[0];
+        return j == numCourses ? result : new int[] {};
     }
 }
