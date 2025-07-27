ff79cca75ea5f784ef4ac808f811dfbec77ca6a8
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-07-27 22:44:29 +0530

    Time: 14 ms (30.74%), Space: 49.2 MB (57.84%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,48 +1,41 @@
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        for (int[] road : roads) {
-            int u = road[0], v = road[1], time = road[2];
-            graph.get(u).add(new int[]{v, time});
-            graph.get(v).add(new int[]{u, time});
-        }
-
-        long[] dist = new long[n];
-        int[] ways = new int[n];
-        Arrays.fill(dist, Long.MAX_VALUE);
-        dist[0] = 0;
-        ways[0] = 1;
-
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
-        pq.offer(new long[]{0, 0});
-
         int MOD = 1_000_000_007;
-
-        while (!pq.isEmpty()) {
-            long[] curr = pq.poll();
-            long d = curr[0];
-            int node = (int) curr[1];
-
-            if (d > dist[node]) continue;
-
-            for (int[] neighbor : graph.get(node)) {
-                int nextNode = neighbor[0];
-                int time = neighbor[1];
-
-                if (dist[node] + time < dist[nextNode]) {
-                    dist[nextNode] = dist[node] + time;
-                    ways[nextNode] = ways[node];
-                    pq.offer(new long[]{dist[nextNode], nextNode});
-                } else if (dist[node] + time == dist[nextNode]) {
-                    ways[nextNode] = (ways[nextNode] + ways[node]) % MOD;
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        long[] time = new long[n];
+        for(int i=0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0; i<roads.length; i++) {
+            adj.get(roads[i][0]).add(new int[] {roads[i][1], roads[i][2]});
+            adj.get(roads[i][1]).add(new int[] {roads[i][0], roads[i][2]});
+        }
+        for(int i=0; i<n; i++) {
+            time[i] = Long.MAX_VALUE; 
+        }
+        // PriorityQueue<long[]> pq = new PriorityQueue<>((x, y) -> x[0] - y[0]);
+        PriorityQueue<long[]> pq = new PriorityQueue<>((x, y) -> Long.compare(x[0], y[0]));
+        pq.offer(new long[] {0L, 0});
+        time[0] = 0;
+        int[] countWays = new int[n];
+        countWays[0] = 1;
+        while(!pq.isEmpty()) {
+            long[] arr = pq.poll();
+            int node = (int) arr[1];
+            long timing = arr[0];
+            for(int j=0; j<adj.get(node).size(); j++) {
+                int adjNode = adj.get(node).get(j)[0];
+                int currTime = adj.get(node).get(j)[1];
+                if(timing + currTime < time[adjNode]) {
+                    time[adjNode] = timing + currTime;
+                    pq.offer(new long[] {time[adjNode], adjNode});
+                    countWays[adjNode] = countWays[node];
+                }
+                else if(timing + currTime == time[adjNode]) {
+                    countWays[adjNode] = (countWays[adjNode]  + countWays[node]) % MOD;
                 }
             }
         }
-
-        return ways[n - 1];
+        return countWays[n-1] % MOD;
     }
 }
