77b01ee75b6e374c75de28ee9192cd48b7750d76
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-08-09 00:22:54 +0530

    Time: 0 ms (100%), Space: 40.2 MB (96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,38 @@
+// Memoization
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         return allPaths(m-1, n-1, dp);
+//     }
+//     public int allPaths(int m, int n, int[][] dp) {
+//         if(m == 0 && n == 0) {
+//             return 1;
+//         }
+//         if(m < 0 || n < 0) {
+//             return 0;
+//         }
+//         if(dp[m][n] != 0) return dp[m][n];
+//         return dp[m][n] = allPaths(m-1, n, dp) + allPaths(m, n-1, dp);
+//     }
+// }
+
+// Tabulation
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return allPaths(m-1, n-1, dp);
-    }
-    public int allPaths(int m, int n, int[][] dp) {
-        if(m == 0 && n == 0) {
-            return 1;
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(dp[i][j] == 0) {
+                    if(i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    }
+                    else {
+                        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    }
+                }
+            }
         }
-        if(m < 0 || n < 0) {
-            return 0;
-        }
-        if(dp[m][n] != 0) return dp[m][n];
-        return dp[m][n] = allPaths(m-1, n, dp) + allPaths(m, n-1, dp);
+        return dp[m-1][n-1];
     }
 }
