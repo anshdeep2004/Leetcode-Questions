03bfb9da1e0de37633b657e49baa81f03ab97754
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-08-17 00:25:04 +0530

    Time: 18 ms (89.64%), Space: 45.5 MB (90.42%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -58,59 +58,60 @@
 
 
 // Tabulation using the shifted code (the reason being that in tabulation we have the dp array stired and it cannot have negative values so its important to shift the parameters).
-class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int[][] dp = new int[n+1][m+1];
-        
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = 0;
-        }
-        for(int j=0; j<=m; j++) {
-            dp[0][j] = 0;
-        }
-
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
-                if(text1.charAt(i-1) == text2.charAt(j-1)) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
-    }
-}
-
-
-
-
-// Space optimisation
 // class Solution {
 //     public int longestCommonSubsequence(String text1, String text2) {
 //         int n = text1.length();
 //         int m = text2.length();
-//         int[] prev = new int[m+1];
-//         int[] curr = new int[m+1];
+//         int[][] dp = new int[n+1][m+1];
         
-//         for(int i=0; i<=m; i++) {
-//             prev[i] = 0;
+//         for(int i=0; i<=n; i++) {
+//             dp[i][0] = 0;
+//         }
+//         for(int j=0; j<=m; j++) {
+//             dp[0][j] = 0;
 //         }
 
 //         for(int i=1; i<=n; i++) {
 //             for(int j=1; j<=m; j++) {
 //                 if(text1.charAt(i-1) == text2.charAt(j-1)) {
-//                     curr[j] = 1 + prev[j-1];
+//                     dp[i][j] = 1 + dp[i-1][j-1];
 //                 }
 //                 else {
-//                     curr[j] = Math.max(prev[j], curr[j-1]);
+//                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 //                 }
 //             }
-//             prev = curr;
 //         }
-//         return prev[m];
+//         return dp[n][m];
 //     }
 // }
+
+
+
+
+// Space optimisation - does not work that efficient
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
+        
+        for(int i=0; i<=m; i++) {
+            prev[i] = 0;
+        }
+
+        for(int i=1; i<=n; i++) {
+            curr = new int[m+1];
+            for(int j=1; j<=m; j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    curr[j] = 1 + prev[j-1];
+                }
+                else {
+                    curr[j] = Math.max(prev[j], curr[j-1]);
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+}
