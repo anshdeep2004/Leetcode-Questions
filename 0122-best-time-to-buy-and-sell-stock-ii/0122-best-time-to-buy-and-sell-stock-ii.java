ce410f13a359c52b1d5cda408f2344a89f8a1eb7
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-08-27 20:41:15 +0530

    Time: 2 ms (15.28%), Space: 45.8 MB (72.63%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -35,27 +35,58 @@
 
 
 // Tabulation
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int buy = 1;
+//         int[][] dp = new int[n+1][2];
+
+//         dp[n][0] = dp[n][1] = 0;
+
+//         for(int i=n-1; i>=0; i--) {
+//             for(int b=0; b<2; b++) {
+//                 int profit = 0;
+//                 if(b == 1) {
+//                     profit = Math.max(-prices[i] + dp[i+1][0], 0 + dp[i+1][1]);
+//                 }
+//                 else {
+//                     profit = Math.max(prices[i] + dp[i+1][1], 0 + dp[i+1][0]);
+//                 }
+//                 dp[i][b] = profit;
+//             }
+//         }
+
+//         return dp[0][1];
+//     }
+// }
+
+
+
+
+// Space optimisation
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int buy = 1;
-        int[][] dp = new int[n+1][2];
+        int[] ahead = new int[2];
+        int[] curr = new int[2];
 
-        dp[n][0] = dp[n][1] = 0;
+        ahead[0] = ahead[1] = 0;
 
         for(int i=n-1; i>=0; i--) {
             for(int b=0; b<2; b++) {
                 int profit = 0;
                 if(b == 1) {
-                    profit = Math.max(-prices[i] + dp[i+1][0], 0 + dp[i+1][1]);
+                    profit = Math.max(-prices[i] + ahead[0], 0 + ahead[1]);
                 }
                 else {
-                    profit = Math.max(prices[i] + dp[i+1][1], 0 + dp[i+1][0]);
+                    profit = Math.max(prices[i] + ahead[1], 0 + ahead[0]);
                 }
-                dp[i][b] = profit;
+                curr[b] = profit;
             }
+            ahead = Arrays.copyOf(curr, curr.length);
         }
 
-        return dp[0][1];
+        return ahead[1];
     }
 }
