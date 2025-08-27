dfd332b51bdaa871ae2c5cc4c735056aa32c6a89
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-08-27 11:26:11 +0530

    Time: 27 ms (8.91%), Space: 49.1 MB (7.86%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,35 +1,79 @@
 // Memoization
+// class Solution {
+//     public boolean match(int i, int j, String p, String s, Boolean[][] dp) {
+//         if(i < 0 && j < 0) return true;
+//         if(i < 0) return false;
+//         if(j < 0) {
+//             for(int k=0; k<=i; k++) {
+//                 if(p.charAt(k) != '*') return false;
+//             }
+//             return true;
+//         }
+
+//         if(dp[i][j] != null) return dp[i][j];
+
+//         if(p.charAt(i) == s.charAt(j) || p.charAt(i) == '?') {
+//             return dp[i][j] = match(i-1, j-1, p, s, dp);
+//         }
+//         else if(p.charAt(i) == '*') {
+//             return dp[i][j] = match(i-1, j, p, s, dp) | match(i, j-1, p, s, dp);
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+
+//     public boolean isMatch(String s, String p) {
+//         int n = s.length();
+//         int m = p.length();
+
+//         Boolean[][] dp = new Boolean[m][n];
+        
+//         // the match function code is written such that that we are considering the ? and * string as the first string do in the parameters we are first sending p and then s and same goes with their lengths also
+//         return match(m-1, n-1, p, s, dp);
+//     }
+// }
+
+
+// Tabulation - shift by 1 
 class Solution {
-    public boolean match(int i, int j, String p, String s, Boolean[][] dp) {
-        if(i < 0 && j < 0) return true;
-        if(i < 0) return false;
-        if(j < 0) {
-            for(int k=0; k<=i; k++) {
-                if(p.charAt(k) != '*') return false;
-            }
-            return true;
-        }
-
-        if(dp[i][j] != null) return dp[i][j];
-
-        if(p.charAt(i) == s.charAt(j) || p.charAt(i) == '?') {
-            return dp[i][j] = match(i-1, j-1, p, s, dp);
-        }
-        else if(p.charAt(i) == '*') {
-            return dp[i][j] = match(i-1, j, p, s, dp) | match(i, j-1, p, s, dp);
-        }
-        else {
-            return false;
-        }
-    }
-
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
 
-        Boolean[][] dp = new Boolean[m][n];
-        
-        // the match function code is written such that that we are considering the ? and * string as the first string do in the parameters we are first sending p and then s and same goes with their lengths also
-        return match(m-1, n-1, p, s, dp);
+        Boolean[][] dp = new Boolean[m+1][n+1];
+
+        // Base cases
+        dp[0][0] = true;
+
+        for(int j=1; j<=n; j++) {
+            dp[0][j] = false;
+        }
+
+        for(int i=1; i<=m; i++) {
+            boolean flag = true;
+            for(int k=1; k<=i; k++) {
+                if(p.charAt(k-1) != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+
+        // traversing in dp array
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(p.charAt(i-1) == s.charAt(j-1) || p.charAt(i-1) == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p.charAt(i-1) == '*') {
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                }
+                else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 }
