2ed34d84ac2ea611b50ca4dfd8ca4003ddd6a234
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-09-13 13:18:13 +0530

    Time: 73 ms (28.36%), Space: 77.2 MB (22.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,48 @@
 // Memoization
-class Solution {
-    public int f(int idx, int prevIdx, int n, int[] nums, int [][] dp) {
-        if(idx == n) return 0;
+// class Solution {
+//     public int f(int idx, int prevIdx, int n, int[] nums, int [][] dp) {
+//         if(idx == n) return 0;
         
-        if(dp[idx][prevIdx+1] != -1) return dp[idx][prevIdx+1];
+//         if(dp[idx][prevIdx+1] != -1) return dp[idx][prevIdx+1];
 
-        int lenNotTake = 0 + f(idx+1, prevIdx, n, nums, dp);
-        int lenTake = 0;
-        if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
-            lenTake = 1 + f(idx+1, idx, n, nums, dp);
-        }
+//         int lenNotTake = 0 + f(idx+1, prevIdx, n, nums, dp);
+//         int lenTake = 0;
+//         if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+//             lenTake = 1 + f(idx+1, idx, n, nums, dp);
+//         }
 
-        return dp[idx][prevIdx+1] = Math.max(lenTake, lenNotTake);
-    }
+//         return dp[idx][prevIdx+1] = Math.max(lenTake, lenNotTake);
+//     }
 
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int[][] dp = new int[n][n+1];
+//         for(int i=0; i<n; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+
+//         return f(0, -1, n, nums, dp);
+//     }
+// }
+
+
+// Tabulation
+class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
-        for(int i=0; i<n; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[n+1][n+1];
+
+        for(int idx=n-1; idx>=0; idx--) {
+            for(int prevIdx=idx-1; prevIdx>=-1; prevIdx--) {
+                int lenNotTake = 0 + dp[idx+1][prevIdx+1];
+                int lenTake = 0;
+                if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+                    lenTake = 1 + dp[idx+1][idx+1];
+                }
+                dp[idx][prevIdx+1] = Math.max(lenTake, lenNotTake);
+            }
         }
 
-        return f(0, -1, n, nums, dp);
+        return dp[0][-1+1];
     }
 }
