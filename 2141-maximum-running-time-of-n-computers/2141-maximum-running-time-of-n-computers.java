be0234e1542d0f1e1984647a7899138513b3726e
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2025-12-26 18:25:13 +0530

    Time: 16 ms (73.81%), Space: 74.2 MB (6.57%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,8 +1,31 @@
+// To run all n computers for T minutes:
+
+// Total energy required = n × T
+
+// Each battery can contribute at most min(battery[i], T) minutes
+
+// Why min(battery[i], T)?
+
+// A battery cannot be used for more than T minutes overall
+
+// Even if a battery has more energy, it can only power one computer at a time
+
+// Feasibility Condition
+
+// For a given time T, check:
+// 	​sum(min(battery[i], t)) >= n * T
+
+// If this is true → it is possible to run all computers for T minutes
+// If false → not possible
+
+// This is the core condition of the entire problem.
+
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
         int len = batteries.length;
         long left = 0;
-        // int right = (int) 1e9;
+        // int right = (int) 1e9; ---> this is not good for complexity. Below is a better method.
+        // upper bound will be sum(Batteries)/n as we cannot run the n computers more than average time for each computer.
         long totalEnergy = 0;
         for (int b : batteries) {
             totalEnergy += b;
