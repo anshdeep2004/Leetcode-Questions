5a08b1bb4ee1ab5bdd2abb1c2d9d667d4c05d95f
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2026-04-30 16:56:13 +0530

    Time: 2435 ms (8.23%), Space: 35.8 MB (24.74%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isSubsetSum(self, arr: List[int], target) -> bool:
+        n = len(arr)
+        # dp = np.zeros((n + 1, target + 1), dtype = bool)
+        dp = [[False for _ in range(target + 1)] for _ in range(n + 1)]
+
+        # base case
+        for i in range(0, n + 1):
+            dp[i][0] = True
+
+        for i in range(1, n + 1):
+            for j in range(1, target + 1):
+                take = False
+                if(j >= arr[i-1]):
+                    take = dp[i-1][j - arr[i-1]]
+                notTake = dp[i-1][j]
+                dp[i][j] = take | notTake
+        return dp[n][target]
+
+    def canPartition(self, nums: List[int]) -> bool:
+        totalSum = 0
+        for i in range(0, len(nums)):
+            totalSum += nums[i]
+
+        if totalSum % 2 != 0:
+            return False
+        
+        return self.isSubsetSum(nums, totalSum // 2)
+
+        
