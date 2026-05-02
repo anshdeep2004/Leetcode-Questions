160353806ea6ed3bb8d612347df4038b44dfbe04
anshdeep2004 <150055140+anshdeep2004@users.noreply.github.com>
2026-05-02 20:12:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,44 +1,15 @@
-# class Solution:
-#     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-#         n = len(nums)
-#         totalSum = sum(nums)
-#         if (totalSum - target) % 2 != 0:
-#             return 0
-#         if target > totalSum:
-#             return 0
-#         s2 = (totalSum - target) // 2
-#         dp = [[0 for _ in range(s2 + 1)] for _ in range(n)]
-
-#         # base cases
-#         for i in range(0, n):
-#             dp[i][0] = 1
-#         if nums[0] <= s2:
-#             dp[0][nums[0]] = 1
-
-#         # DP for loops
-#         for i in range(1, n):
-#             for j in range(1, s2 + 1):
-#                 take = 0
-#                 if j >= nums[i]:
-#                     take = dp[i-1][j - nums[i]]
-#                 notTake = dp[i-1][j]
-#                 dp[i][j] = take + notTake
-        
-#         return dp[n-1][s2]
-
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
         totalSum = sum(nums)
-
-        if totalSum - target < 0 or (totalSum - target) % 2 != 0:
+        if (totalSum - target) % 2 != 0:
             return 0
-
+        if target > totalSum:
+            return 0
         s2 = (totalSum - target) // 2
-
         dp = [[0 for _ in range(s2 + 1)] for _ in range(n)]
 
-        # base case
+        # base cases
         if nums[0] == 0:
             dp[0][0] = 2
         else:
@@ -47,14 +18,13 @@ class Solution:
         if nums[0] != 0 and nums[0] <= s2:
             dp[0][nums[0]] = 1
 
+        # DP for loops
         for i in range(1, n):
             for j in range(0, s2 + 1):
                 take = 0
-                if nums[i] <= j:
-                    take = dp[i - 1][j - nums[i]]
-
-                notTake = dp[i - 1][j]
-
+                if j >= nums[i]:
+                    take = dp[i-1][j - nums[i]]
+                notTake = dp[i-1][j]
                 dp[i][j] = take + notTake
-
-        return dp[n - 1][s2]
+        
+        return dp[n-1][s2]
